fe4eb403bcac33053fc2a3b258d3cdb90346ddbb
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-10-17 09:36:26 +0700

    create service component for home page

---
+++ components/Service/Service.tsx
@@ -0,0 +1,38 @@
+const services = [
+  {
+    service: "Original Guaranteed",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, error? lorem",
+  },
+  {
+    service: "Free shipping",
+    description: "Lorem ipsum dolor sit,  elit. Placeat, error?",
+  },
+  {
+    service: "Easy Payment",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, error?",
+  },
+  {
+    service: "Easy Return",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, error?",
+  },
+];
+
+function Service() {
+  return (
+    <section id="service" className="mx-auto px-6 py-12 md:py-20">
+      <ul className="flex flex-wrap justify-around gap-y-12 lg:gap-y-8">
+        {services.map((service, index) => {
+          return (
+            <li className="align flex w-full flex-col items-center text-center md:w-[45%] lg:w-[20%]" key={index}>
+              <div className="mb-4 text-5xl">😎</div>
+              <h3 className="text-2xl font-medium">{service.service}</h3>
+              <p>{service.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+export default Service;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import HeroCarousel from "../components/carousel/HeroCarousel";
+import HeroCarousel from "../components/Carousel/HeroCarousel/HeroCarousel";
+import Service from "../components/Service/Service";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div id="home" className="pt-20">
       <HeroCarousel />
+      <Service />
     </div>
   );
 };
