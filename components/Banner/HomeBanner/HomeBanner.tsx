7f39291090f81dcffd8b738a14a04374b9b9bbd3
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-10-19 09:34:09 +0700

    create HomeBanner component

---
+++ components/Banner/HomeBanner/HomeBanner.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+type HomeBannerProps = {
+  src: string;
+  heading?: string;
+};
+function HomeBanner({ src, heading }: HomeBannerProps) {
+  return (
+    <div className="relative after:absolute after:left-0 after:right-0 after:top-0 after:bottom-0 after:bg-black after:opacity-40 after:content-['']">
+      <img src={`${src}`} className="w-full" alt="home-banner"></img>
+      <p className="absolute bottom-1/4 left-4 z-[2] text-2xl font-bold uppercase text-white sm:text-4xl md:left-8 md:text-6xl lg:left-12 lg:text-8xl">{heading}</p>
+    </div>
+  );
+}
+
+export default HomeBanner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import HomeBanner from "../components/Banner/HomeBanner/HomeBanner";
 import HeroCarousel from "../components/Carousel/HeroCarousel/HeroCarousel";
 import Service from "../components/Service/Service";
 import styles from "../styles/Home.module.css";
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
     <div id="home" className="pt-20">
       <HeroCarousel />
       <Service />
+      <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
     </div>
   );
 };
