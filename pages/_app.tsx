58309e6caa8d4c249ffc2ace06e72e67bdaf1a37
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-11-21 19:43:59 +0700

    setup react-query queryClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
