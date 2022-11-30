e9ed616874dfa2d7f50c057761332d264b7611ca
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-11-30 20:26:40 +0700

    create add to cart functionality

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { useMutation, useQueryClient } from "react-query";
 import { useRouter } from "next/router";
+import { UserType } from "../../components/Navbar/Navbar";
 
 type ProductDetailsProps = {
   data: {
@@ -23,11 +24,18 @@ type ProductDetailsProps = {
   };
 };
 
+type test = (productId: string, quantity: string) => any;
+
 const ProductDetails: NextPage<ProductDetailsProps> = ({ data }) => {
   const router = useRouter();
+
+  //Get productId from params
   const { productId } = router.query;
   const queryClient = useQueryClient();
+
   const [productAmount, setProductAmount] = useState(1);
+
+  //Increment and decrement product handler
   const incrementAmount = () => {
     setProductAmount((prevAmount) => prevAmount + 1);
   };
@@ -35,14 +43,16 @@ const ProductDetails: NextPage<ProductDetailsProps> = ({ data }) => {
     setProductAmount((prevAmount) => prevAmount - 1);
   };
 
+  //Error state
   const [wishlistErrMsg, setWishlistErrMsg] = useState("");
+  const [cartErrMsg, setCartErrMsg] = useState("");
 
   const {
     data: addwishlistResponse,
     isError: addWishlistError,
     isLoading: addWishlistLoading,
     mutate: sendAddWishlistRequest,
-  } = useMutation<any, any, string | undefined>({
+  } = useMutation<any, any, string>({
     mutationFn: async (productId) => {
       const response = await axios.post("http://localhost:5000/api/v1/wishlist", { productId }, { withCredentials: true });
       const data = response.data;
@@ -74,6 +84,48 @@ const ProductDetails: NextPage<ProductDetailsProps> = ({ data }) => {
     }
   };
 
+  const {
+    data: addCartResponse,
+    isLoading: addCartLoading,
+    error: addCartError,
+    isError: isAddCartError,
+    mutate: sendAddCartRequest,
+  } = useMutation<any, any, { productId: string; quantity: string }>({
+    mutationFn: async ({ productId, quantity }) => {
+      const response = await axios.post("http://localhost:5000/api/v1/cart", { productId, quantity }, { withCredentials: true });
+      const data = response.data;
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.setQueryData<UserType | undefined>(["profile"], (oldProfile) => {
+        if (oldProfile) {
+          return {
+            ...oldProfile,
+            cartCount: oldProfile.cartCount + 1,
+          };
+        }
+        return oldProfile;
+      });
+
+      setProductAmount(1);
+      // setIsCartModalActive((prev) => !prev);
+      setCartErrMsg("");
+    },
+    onError: (addCartError) => {
+      if (addCartError?.response?.data?.message === "Duplicate value error") {
+        setCartErrMsg("Item is already in Cart");
+      } else {
+        setCartErrMsg("Something went wrong please try again later");
+      }
+    },
+  });
+
+  const addToCartHandler = () => {
+    setCartErrMsg("");
+    const tempProductId = productId as string;
+    sendAddCartRequest({ productId: tempProductId, quantity: productAmount.toString() });
+  };
+
   return (
     <section className="bg-slate-50 px-6 pt-20 pb-6">
       <div className="flex flex-wrap">
@@ -96,12 +148,23 @@ const ProductDetails: NextPage<ProductDetailsProps> = ({ data }) => {
           </div>
           <button
             className="mt-4 flex h-14 w-full items-center justify-center border-2 border-black bg-primary uppercase text-white transition-colors duration-300"
-            // onClick={() => {
-            //   addToCartHandler();
-            // }}
+            onClick={() => {
+              addToCartHandler();
+            }}
           >
             add to cart
           </button>
+          {cartErrMsg ? (
+            <span className="!mt-2 block text-red-500">
+              Item is already in cart
+              {/* <Link to="/cart" className="text-black underline">
+                click here
+              </Link>{" "}
+              to check */}
+            </span>
+          ) : (
+            ""
+          )}
 
           <button
             className="mt-4 flex h-14 w-full items-center justify-center border-2 border-black bg-white uppercase text-primary transition-colors duration-300"
