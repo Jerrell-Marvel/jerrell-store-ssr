0b757bc2fbb8e60e0959519f7663f366e9eac090
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-12-10 14:09:48 +0700

    show different products on each carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -7,24 +7,6 @@ import HeroCarousel from "../components/Carousel/HeroCarousel/HeroCarousel";
 import ProductsCarousel from "../components/Carousel/ProductsCarousel/ProductsCarousel";
 import Service from "../components/Service/Service";
 import styles from "../styles/Home.module.css";
-
-const Home: NextPage<HomeProps> = ({ carouselProducts }) => {
-  return (
-    <div id="home" className="pt-20">
-      <HeroCarousel />
-      <Service />
-
-      <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
-      <ProductsCarousel carouselProducts={carouselProducts} />
-
-      <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
-      <ProductsCarousel carouselProducts={carouselProducts} />
-
-      <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
-      <ProductsCarousel carouselProducts={carouselProducts} />
-    </div>
-  );
-};
 
 type HomeProps = {
   carouselProducts: {
@@ -43,13 +25,47 @@ type HomeProps = {
           image: string;
         }[]
       | null;
-  };
+  }[];
 };
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+const Home: NextPage<HomeProps> = ({ carouselProducts }) => {
+  return (
+    <div id="home" className="pt-20">
+      <HeroCarousel />
+      <Service />
+      {carouselProducts.map((carouselProduct, idx) => {
+        return (
+          <div key={idx}>
+            <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
+            <ProductsCarousel carouselProducts={carouselProduct} />
+          </div>
+        );
+      })}
+
+      {/* <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
+      <ProductsCarousel carouselProducts={carouselProducts} />
+
+      <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
+      <ProductsCarousel carouselProducts={carouselProducts} />
+
+      <HomeBanner src="https://source.unsplash.com/random/1600x800" heading="lorem ipsum" />
+      <ProductsCarousel carouselProducts={carouselProducts} /> */}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/api/v1/products?sort=newest");
-    const data = response.data as HomeProps["carouselProducts"];
+    const carouselProductsPromises: Promise<AxiosResponse<HomeProps["carouselProducts"], any>>[] = [];
+    for (let i = 1; i <= 3; i++) {
+      const url = `http://localhost:5000/api/v1/products?sort=newest&page=${i}`;
+      const promise: Promise<AxiosResponse<HomeProps["carouselProducts"], any>> = axios.get(url);
+      carouselProductsPromises.push(promise);
+    }
+
+    const responses = await Promise.all(carouselProductsPromises);
+    const data = responses.map((response) => response.data);
+
     return {
       props: {
         carouselProducts: data,
