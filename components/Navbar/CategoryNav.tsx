ad4489cbe9162bf70c84994e2bc0b8280b1408cb
Jerrell Marvel <jerrell.marvel88@gmail.com>
2022-12-17 10:32:31 +0700

    create CategoryNav

---
+++ components/Navbar/CategoryNav.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+const categoryLinks = ["all", "hoodie", "snacks", "jeans", "shorts", "shirts"];
+const CategoryNav = () => {
+  return (
+    <>
+      <div className="relative flex h-[40vh] w-full items-center justify-center bg-[url('https://source.unsplash.com/random/800x300')] bg-cover px-6 text-center after:absolute after:top-0 after:left-0 after:right-0 after:bottom-0 after:bg-slate-800 after:opacity-40 after:content-[''] mt-20">
+        <h2 className="z-[2] text-5xl font-medium text-white sm:leading-[6rem]">Explore Products</h2>
+      </div>
+      <div className="sticky top-20 z-[50] bg-white">
+        <ul className="flex h-16 items-center justify-between gap-8 overflow-auto border-b-2 bg-white px-6 lg:justify-evenly">
+          {categoryLinks.map((category, index) => {
+            return (
+              <li className="h-full" key={index}>
+                <Link href={`/product-category/${category}`} className="group flex h-full items-center">
+                  <a className="group flex h-full items-center">
+                    <span className="relative w-fit font-medium uppercase after:absolute after:-bottom-1/4 after:left-1/2 after:block after:h-[3px]  after:w-0 after:bg-slate-800 after:transition-all after:duration-300 after:content-[''] group-hover:after:left-0 group-hover:after:right-0 group-hover:after:w-full">
+                      {category}
+                    </span>
+                  </a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default CategoryNav;
